714ce38bef5c528fc637f6945aa6ab46b8227281
quangquan <leo.langlois2@gmail.com>
2023-11-21 17:43:16 +0100

    add app

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 import { Date_now } from "./model.js";
+import { articles } from "./template.js";
 
 document.addEventListener("DOMContentLoaded", e => {
     console.log(e.target);
     Date_now.builder();
     Date_now.el.innerText = Date_now.dates;
     Date_now.el.setAttribute("datetime", Date_now.dates);
+
+    console.table(articles);
 });
---
+++ js/template.js
@@ -0,0 +1,16 @@
+export const articles = [{
+    id: "0",
+    title: "Php and POO",
+    content: "La programmation orientée objet (POO) est devenue indispensable, oui, mais pourquoi ? Parce qu'elle permet une meilleure organisation de vos projets, facilite la maintenance de votre code, et offre une grande souplesse pour faire évoluer votre logiciel sans avoir à tout réécrire",
+    link: "https://openclassrooms.com/fr/courses/1665806-programmez-en-oriente-objet-en-php/7306872-decouvrez-les-objets-et-les-classes"
+}, {
+    id: "1",
+    title: "Php and Laravel",
+    content: "Laravel est un framework web open-source écrit en PHP respectant le principe modèle-vue-contrôleur et entièrement développé en programmation orientée objet. Laravel est distribué sous licence MIT, avec ses sources hébergées sur GitHub",
+    link: "https://fr.wikipedia.org/wiki/Laravel"
+}, {
+    id: "2",
+    title: "Node js",
+    content: "Node.js est une plateforme logicielle libre en JavaScript, orientée vers les applications réseau évènementielles hautement concurrentes qui doivent pouvoir monter en charge.",
+    link: "https://fr.wikipedia.org/wiki/Node.js"
+}];
